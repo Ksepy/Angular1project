b2eca2ca8cf20ec98c01b009c7e2fddb0a45e50f
Ksepy <www.makar.com@gmail.com>
2020-12-13 15:16:59 +0200

    Initial commit

---
+++ my-app/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './book/book.component';
+import { TestPageComponent } from './test-page/test-page.component';
+import { MainComponent } from './main/main.component';
+import { Stage1Component } from './main/stage1/stage1.component';
+import { Stage2Component } from './main/stage2/stage2.component';
+import { Stage3Component } from './main/stage3/stage3.component';
+
+const routes: Routes = [
+  {path: 'book', component: BookComponent},
+  {path: 'test', component: TestPageComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'main/stage1', component: Stage1Component},
+  {path: 'main/stage2', component: Stage2Component},
+  {path: 'main/stage3', component: Stage3Component}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
